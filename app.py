92906179e3519ba7bd72e260660ab56f53817627
AtanuDeSarkar <108272033+AtanuDeSarkar@users.noreply.github.com>
2024-01-25 14:11:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,17 @@ if 'Virus scan warning' in response.text:
 csv_content = response.text
 df = pd.read_csv(StringIO(csv_content))
 
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import geopandas as gpd
+from shapely.wkt import loads
+import plotly.express as px
+from datetime import datetime
+
+# Assuming df is defined earlier in your code
+# df = ...
+
 # Convert the 'geometry' column to Shapely geometries
 df['geometry'] = df['geometry'].apply(loads)
 
@@ -56,25 +67,15 @@ cities = df['City'].unique()
 app = dash.Dash(__name__)
 server = app.server
 
-# Path to the PNG image
-image_path = r'D:\Prediction Work\Data\Variable data from NCAR\New Data\Plots and csv\Kolkata\colorbar.png'
-
-
-# Function to encode the image to base64
-def encode_image(image_path):
-    with open(image_path, 'rb') as image_file:
-        encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
-    return f'data:image/png;base64,{encoded_image}'
-
-
 # Layout of the app
 app.layout = html.Div([
     # Title and Logo
     html.Div([
+        # html.Img(src="your_logo.png", style={'height': '100px', 'width': '100px'}),
         html.Img(src={'src': 'D:/Dashboard_exp_2024/assets/image.png'}, style={'height': '100px', 'width': '100px'}),
         html.H1("Dashbaord for Ward Level PM 2.5 Prediction for Howrah and Kolkata", style={'text-align': 'center'}),
     ], style={'display': 'flex', 'align-items': 'center'}),
-
+    
     # Text labels and Dropdown for selecting city and Datepicker for selecting date
     html.Div([
         html.Div([
@@ -86,7 +87,7 @@ app.layout = html.Div([
                 style={'width': '50%'}
             ),
         ], style={'width': '48%', 'display': 'inline-block'}),
-
+        
         html.Div([
             html.Label("Select Date:"),
             dcc.DatePickerSingle(
@@ -96,23 +97,13 @@ app.layout = html.Div([
             ),
         ], style={'width': '48%', 'display': 'inline-block'}),
     ], style={'margin-bottom': '20px'}),
-
-    # Graph and Image side by side
-    html.Div(
-        children=[
-            dcc.Graph(
-                id='pm25-map',
-                style={'height': '600px', 'width': '92%', 'display': 'inline-block'}  # Set the height of the Mapbox
-            ),
-            html.Img(
-                src=encode_image(image_path),
-                alt='Your Image',
-                style={'width': '8%', 'display': 'inline-block'},
-            ),
-        ],
-    ),
+    
+    # Plotly Express Map
+    dcc.Graph(
+        id='pm25-map',
+        style={'height': '600px', 'width': '100%'}  # Set the height of the Mapbox
+    )
 ])
-
 
 # Callback to update the available dates based on the selected city and set DatePickerSingle properties
 @app.callback(
@@ -124,14 +115,12 @@ app.layout = html.Div([
 )
 def update_dates_options(selected_city):
     filtered_dates = df[df['City'] == selected_city]['Date'].unique()
-    date_options = [{'label': datetime.strptime(date, '%Y-%m-%d').strftime('%Y-%m-%d'), 'value': date} for date in
-                    filtered_dates]
+    date_options = [{'label': datetime.strptime(date, '%Y-%m-%d').strftime('%Y-%m-%d'), 'value': date} for date in filtered_dates]
     min_date = min(filtered_dates)
     max_date = max(filtered_dates)
     initial_visible_month = min_date
-
+    
     return date_options, min_date, max_date, initial_visible_month
-
 
 # Callback to set the default selected date
 @app.callback(
@@ -141,7 +130,6 @@ def update_dates_options(selected_city):
 def set_default_date(date_options):
     return date_options[0]['value'] if date_options else None
 
-
 # Callback to update the map based on the selected city and date
 @app.callback(
     Output('pm25-map', 'figure'),
@@ -150,7 +138,7 @@ def set_default_date(date_options):
 )
 def update_map(selected_city, selected_date):
     filtered_gdf = gdf[(gdf['City'] == selected_city) & (gdf['Date'] == selected_date)]
-
+    
     fig = px.choropleth_mapbox(
         filtered_gdf,
         geojson=filtered_gdf.geometry.__geo_interface__,
@@ -163,18 +151,16 @@ def update_map(selected_city, selected_date):
         zoom=10.6,
         center={"lat": filtered_gdf.geometry.centroid.y.mean(), "lon": filtered_gdf.geometry.centroid.x.mean()},
         opacity=0.5,
+        
+        
     )
-
+    
     fig.update_traces(
         customdata=filtered_gdf[['WARD', 'PM2.5']],
         hovertemplate='<b>Ward:</b> %{customdata[0]}<br><b>PM2.5:</b> %{customdata[1]:.2f}'
     )
-
-    # Remove color bar
-    fig.update_layout(coloraxis_showscale=False)
-
+    
     return fig
-
 
 # Run the app
 if __name__=='__main__':
